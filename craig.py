7bb9f11c9a47eb87ac38d91c29aa6d9fb3dc4582
looolol <robbierankin15@gmail.com>
2024-08-14 03:09:04 -0400

    changed to use asynchronous requests

--- craig.py
+++ craig.py
@@ -1,6 +1,6 @@
 import discord
 import feedparser
-import requests
+import aiohttp
 from io import BytesIO
 from bs4 import BeautifulSoup
 import os
@@ -14,6 +14,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+
+RATE_LIMIT_STATUS = 429
+
 # Decode HTML entities
 def decode_url(url):
 	return url.replace("&amp;", "&")
@@ -25,68 +28,68 @@ def fetch_rss_feed(url):
 	print('Done!')
 	return feed
 
+async def fetch_image(url):
+	async with aiohttp.ClientSession() as session:
+		async with session.get(url) as response:
+
+			if response.status == 200:
+				return await response.read()
+
+			elif response.status == RATE_LIMIT_STATUS:
+				print("Rate limit hit: Retrying...")
+				async with session.get(url) as response:
+					if retry_response.status == 200:
+						return await retry_response.read()
+			else:
+				print(f"Failed to fetch image: {url}, status code: {response.status}")
+				return None
+
 # Function to extract image URLs from the Reddit post
-def extract_images(entry):
-	print('')
-	print(f'Extracting images...{entry.title} / {entry.link}')
+async def extract_images(entry):
+	print(f'\nExtracting images...{entry.title} / {entry.link}')
 	images = []
 
 	# fetch post json data
 	if entry.link:
 
-		response = requests.get(f"{entry.link}.json")
+		async with aiohttp.ClientSession() as session:
+			async with session.get(f"{entry.link}.json") as response:
 
-		# If reddit api returns rate limit error, then retry till success
-		if response.status_code == 429:
-			print("Rate limit Hit: Retrying...")
+				if response.status == RATE_LIMIT_STATUS:
+					print("Rate limit hit: Retrying...")
+					while response.status == RATE_LIMIT_STATUS:
+						async with session.get(f"{entry.link}.json") as response:
+							pass
 
-			while (response.status_code == 429):
-				response = requests.get(f"{entry.link}.json")
+				if response.status == 200:
+					data = await response.json()
 
-		if response.status_code == 200:
-			data = response.json()
+					try:
+						for obj in data:
+							for child in obj['data']['children']:
+								if child['kind'] == "t3":
 
-
-			# Extract all images from the Reddit posts comment's metadata
-			try:
-				# For all children in the post data
-				for obj in data:
-					for child in obj['data']['children']:
-
-						# Filter out comments
-						if child['kind'] == "t3":
-
-							if 'preview' in child['data']:
-								source = child['data']['preview']['images'][0]['source']
-
-								img_link = decode_url(source['url'])
-								print(f'\timg_link')
-								images.append(img_link)
+									# Handle Preview Images
+									if 'preview' in child['data']:
+										source = child['data']['preview']['images'][0]['source']
+										img_link = decode_url(source['url'])
+										images.append(img_link)
 
 
-							if 'media_metadata' in child['data']:
-								# Look for Image metadata
-								for media_metadata in child['data']['media_metadata'].values():
-
-									# Meta data contains non-descriptive keys
-									# 'E' is media type?, checking for Images
-									if 'e' in media_metadata and media_metadata['e'] == 'Image':
-										# 'e' contains two children, 'p' and 's'.
-										# 'p' have different sizes of the image
-										# 's' I belive is the source picture
-										# 'u' contains the link that is needed
-										if 's' in media_metadata and 'u' in media_metadata['s']:
-											img_link = decode_url(media_metadata['s']['u'])
-											print(f'\timg_link')
-											images.append(img_link)
-									else:
-										print(f"No link found: {media_metadata}")
-			except Exception as e:
-				print(f"Error parsing object: {e}")
-		else:
-			print(f"Bad response: {response.status_code}")
+									# Handle Metadata
+									if 'media_metadata' in child['data']:
+										for media_metadata in child['data']['media_metadata'].values():
+											if media_metadata.get('e') == 'Image':
+												img_link = decode_url(media_metadata['s']['u'])
+												images.append(img_link)
+					except Exception as e:
+						print(f"Error parsing object: {e}")
+				else:
+					print(f"Bad response: {response.status}")
 
 	print(f'Done! count:{len(images)}')
+	for img_url in images:
+		print(f"\tImage URL: {img_url}")
 	return images
 
 async def post_all_to_discord(channel, title, link, images):
@@ -109,22 +112,15 @@ async def post_to_discord(channel, title, link, images):
 	print('\tPosting to discord...')
 
 	files = []
-	i = 0
-	for img in images:
-		response = requests.get(img)
-		if response.status_code == 200:
-			# Create a file-like object
-			image_file = BytesIO(response.content)
-			image_file.name = "image.jpg"
+	for i, img in enumerate(images):
+		img_data = await fetch_image(img)
 
-			# Create a discord.File object
-			file = discord.File(fp=image_file, filename=f"image{i}.jpg")
-			i = i + 1
+		if img_data:
+			image_file = BytesIO(img_data)
+			image_file.name = f"image{i}.jpg"
+			files.append(discord.File(fp=image_file, filename=image_file.name))
 
-			files.append(file)
-
-	
-	await channel.send(title, files=files)
+	await channel.send(content=title, files=files)
 	print('\tDone!')
 
 @client.event
@@ -132,14 +128,13 @@ async def on_ready():
 	print(f'Logged in as {client.user}')
 	channel = client.get_channel(CHANNEL_ID)
 
-	# Example Reddit RSS feed URL (Replace with your desired subreddit)
 	rss_url = 'https://www.reddit.com/search.rss?q=flair%3Aspoiler+%28subreddit%3AmagicTCG%29&include_over_18=on&sort=new&t=all'
 	feed = fetch_rss_feed(rss_url)
 
 	print(f'Num entries: {len(feed.entries)}')
 	    
 	for entry in feed.entries:
-		images = extract_images(entry)
+		images = await extract_images(entry)
 
 		await post_all_to_discord(channel, entry.title, entry.link, images)
 
