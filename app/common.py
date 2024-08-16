70a193e3087cd07ad1ff59bd2702b189c9a9e712
looolol <robbierankin15@gmail.com>
2024-08-15 22:11:37 -0400

    changed logging to show in console and save as file

--- app/common.py
+++ app/common.py
@@ -4,25 +4,34 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 
 ID_FILE = os.getenv('ID_FILE')
+
 LOG_FILE = os.getenv('LOG_FILE')
+log_msg_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 RATE_LIMIT_STATUS = int(os.getenv('RATE_LIMIT_STATUS'))
 
 
 def setup_logging(name=None, level=logging.INFO, filename=LOG_FILE, when='midnight', interval=1, backup_count=7):
     logger = logging.getLogger(name)
+    logger.setLevel(level)  # Set the log level for the logger
+
     if not logger.hasHandlers():
+        # Console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)  # Set level for console output
+        console_formatter = logging.Formatter(log_msg_format)
+        console_handler.setFormatter(console_formatter)
+        logger.addHandler(console_handler)
+
         if filename:
-            handler = TimedRotatingFileHandler(
+            # File Handler
+            file_handler = TimedRotatingFileHandler(
                 filename, when=when, interval=interval, backupCount=backup_count
             )
-        else:
-            handler = logging.StreamHandler()
-
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(level)
+            file_handler.setLevel(level)  # Set level for file output
+            file_formatter = logging.Formatter(log_msg_format)
+            file_handler.setFormatter(file_formatter)
+            logger.addHandler(file_handler)
 
     return logger
 
